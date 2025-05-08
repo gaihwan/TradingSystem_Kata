f5c1eceacc8f270560d12619d0afd052a5c92d1b
monica kang <aksenwkd92@naver.com>
2025-05-08 14:07:08 +0900

    MockAPI class 생성

--- TradingSystem_Kata/main.cpp
+++ TradingSystem_Kata/main.cpp
@@ -2,6 +2,20 @@
 #include "kiwer_api.cpp"
 #include "nemo_api.cpp"
 
+using namespace testing;
+
+class MockAPI : public StockBrokerDriverInterface {
+public: 
+    MOCK_METHOD(void, doLogin, (std::string ID, std::string password), (override));
+    MOCK_METHOD(void, buyStock, (std::string stockCode, int count, int price), (override));
+    MOCK_METHOD(void, sellStock, (std::string stockCode, int count, int price), (override));
+    MOCK_METHOD(int, getCurrentPrice, (std::string stockCode), (override));
+
+    MOCK_METHOD(void, buyNiceTiming, (std::string stockCode, int price), (override));
+    MOCK_METHOD(void, sellNiceTiming, (std::string stockCode, int count), (override));
+    MOCK_METHOD(int, getMarketPriceInMinutue, (std::string stockCode, int minute), (override));
+};
+
 TEST(TradingSystemTS, CreateApplicationClass) {
     Application* app = nullptr;
     EXPECT_EQ(app, nullptr);
