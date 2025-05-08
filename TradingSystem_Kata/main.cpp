a2dd04d122c1d569989053938b013a23a84e67a9
Janghwan.K <gaihwan@gmail.com>
2025-05-08 13:04:25 +0900

    UnitTest Initial Commitgit status

--- TradingSystem_Kata/main.cpp
+++ TradingSystem_Kata/main.cpp
@@ -2,6 +2,11 @@
 #include "kiwer_api.cpp"
 #include "nemo_api.cpp"
 
+TEST(TradingSystemTS, CreateApplicationClass) {
+    Application* app = nullptr;
+    EXPECT_EQ(app, nullptr);
+}
+
 int main() {
     ::testing::InitGoogleMock();
     return RUN_ALL_TESTS();
