954c31203484ed2840fefc585b2455346adf5818
Janghwan.K <gaihwan@gmail.com>
2025-05-08 15:23:32 +0900

    after rebase

--- TradingSystem_Kata/main.cpp
+++ TradingSystem_Kata/main.cpp
@@ -2,12 +2,13 @@
 #include "kiwer_api.cpp"
 #include "nemo_api.cpp"
 #include "StockerBrokerDriver.h"
-#include "MockAdapter.h"
+#include "MockApi.h"
 
 #include <iostream>
 #include <string>
 
-class MockDriver : public MockAdapter {
+class MockDriver : public MockApi, public StockerBrokerDriverInterface {
+public:
     MOCK_METHOD(void, doLogin, (std::string ID, std::string password), (override));
     MOCK_METHOD(void, buyStock, (std::string stockCode, int count, int price), (override));
     MOCK_METHOD(void, sellStock, (std::string stockCode, int count, int price), (override));
@@ -15,22 +16,37 @@ class MockDriver : public MockAdapter {
     MOCK_METHOD(int, getMarketPriceInMinutue, (std::string stockCode, int minute), (override));
 };
 
-//TEST(TradingSystemTS, CreateApplicationClass) {
-//    Application* app = nullptr;
-//    EXPECT_EQ(app, nullptr);
+TEST(MockDriverTS, CreateMockDriverTC) {
+    MockDriver* mock = new MockDriver;
+    EXPECT_NE(&mock, nullptr);
+}
+
+TEST(MockDriverTC, LoginMockDriverTC) {
+    MockDriver mock;
+    EXPECT_CALL(mock, doLogin("Anonymous", "1234"));
+    mock.doLogin("Anonymous", "1234");
+}
+
+TEST(MockDriver, SellMockDriver) {
+    MockDriver mock;
+    EXPECT_CALL(mock, sellStock("00700", 100, 10000));
+
+    //MockAdapter adapter{ &mock }
+    mock.sellStock("00700", 100, 10000);
+}
+
+TEST(MockDriver, buyMockDriver) {
+    MockDriver mock;
+    EXPECT_CALL(mock, buyStock("00700", 500, 12000));
+
+    //MockAdapter adapter{ &mock }
+    mock.buyStock("00700", 500, 12000);
+}
+
+//TEST(StockBrockerDriverInterface, CreateStockBrockerInterface) {
+//    StockerBrokerDriverInterface* brockerInterface = new StockerBrokerDriverInterface();
+//    EXPECT_NE(&brockerInterface, nullptr); 
 //}
-//
-//
-
-TEST(MockDriver, CreateMockDriver) {
-    MockDriver* mockAPI = new MockDriver;
-    EXPECT_NE(&mockAPI, nullptr);
-}
-
-TEST(StockBrockerDriverInterface, CreateStockBrockerInterface) {
-    StockerBrokerDriverInterface* brockerInterface = new MockDriver();
-    EXPECT_NE(&brockerInterface, nullptr); 
-}
 
 int main() {
     ::testing::InitGoogleMock();
